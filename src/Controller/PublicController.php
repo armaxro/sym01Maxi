872131f3a4f242676e5fde31fac8e216b5e0e18e
Maxi Rojas <arnaldomrojas@outlook.com>
2024-09-02 13:25:21 +0200

    exe

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -8,10 +8,31 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PublicController extends AbstractController
 {
-    #[Route('/public', name: 'app_public')]
+    #[Route('/', name: 'homepage')]
     public function index(): Response
     {
         return $this->render('public/index.html.twig', [
+            'controller_name' => 'PublicController',
+        ]);
+    }
+    #[Route('/page/{id?1}', name: 'page')]
+    public function page(int $id): Response
+    {
+        return $this->render('public/page1.html.twig', [
+            'controller_name' => 'PublicController',
+        ]);
+    }
+    #[Route('/haha', name: 'haha')]
+    public function haha(): Response
+    {
+        return $this->render('public/haha.html.twig', [
+            'controller_name' => 'PublicController',
+        ]);
+    }
+    #[Route('/contact', name: 'contact')]
+    public function contact(): Response
+    {
+        return $this->render('public/contact.html.twig', [
             'controller_name' => 'PublicController',
         ]);
     }
